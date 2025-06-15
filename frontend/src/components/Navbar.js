30909a85567583907fb40882f86e12645bb142f0
Joshua Rahul Tan Sreedharan <joshua.rahul1707@gmail.com>
2025-06-15 20:36:38 +0800

    Fixed App.css path

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { signOut } from 'firebase/auth';
 import { auth } from '../firebase';
-import './App.css';
+import '../App.css';
 
 const Navbar = ({ user }) => {
   const navigate = useNavigate();
