a7a4df69556f1b91010593e893ddeb013af5f539
Joshua <joshua.rahul1707@gmail.com>
2025-07-28 02:17:33 +0800

    Fixed team mapping

--- backend/model_utils.py
+++ backend/model_utils.py
@@ -4,36 +4,16 @@ import os
 
 # Team abbreviations dictionary
 TEAM_ABBREVIATIONS = {
-    "ATL": "Atlanta Hawks",
-    "BOS": "Boston Celtics",
-    "BKN": "Brooklyn Nets",
-    "CHA": "Charlotte Hornets",
-    "CHI": "Chicago Bulls",
-    "CLE": "Cleveland Cavaliers",
-    "DAL": "Dallas Mavericks",
-    "DEN": "Denver Nuggets",
-    "DET": "Detroit Pistons",
-    "GSW": "Golden State Warriors",
-    "HOU": "Houston Rockets",
-    "IND": "Indiana Pacers",
-    "LAC": "Los Angeles Clippers",
-    "LAL": "Los Angeles Lakers",
-    "MEM": "Memphis Grizzlies",
-    "MIA": "Miami Heat",
-    "MIL": "Milwaukee Bucks",
-    "MIN": "Minnesota Timberwolves",
-    "NOP": "New Orleans Pelicans",
-    "NYK": "New York Knicks",
-    "OKC": "Oklahoma City Thunder",
-    "ORL": "Orlando Magic",
-    "PHI": "Philadelphia 76ers",
-    "PHX": "Phoenix Suns",
-    "POR": "Portland Trail Blazers",
-    "SAC": "Sacramento Kings",
-    "SAS": "San Antonio Spurs",
-    "TOR": "Toronto Raptors",
-    "UTA": "Utah Jazz",
-    "WAS": "Washington Wizards"
+    "ATL": "ATLANTA HAWKS", "BOS": "BOSTON CELTICS", "BKN": "BROOKLYN NETS",
+    "CHA": "CHARLOTTE HORNETS", "CHI": "CHICAGO BULLS", "CLE": "CLEVELAND CAVALIERS",
+    "DET": "DETROIT PISTONS", "IND": "INDIANA PACERS", "MIA": "MIAMI HEAT",
+    "MIL": "MILWAUKEE BUCKS", "NYK": "NEW YORK KNICKS", "ORL": "ORLANDO MAGIC",
+    "PHI": "PHILADELPHIA 76ERS", "TOR": "TORONTO RAPTORS", "WAS": "WASHINGTON WIZARDS",
+    "DAL": "DALLAS MAVERICKS", "DEN": "DENVER NUGGETS", "GSW": "GOLDEN STATE WARRIORS",
+    "HOU": "HOUSTON ROCKETS", "LAC": "LOS ANGELES CLIPPERS", "LAL": "LOS ANGELES LAKERS",
+    "MEM": "MEMPHIS GRIZZLIES", "MIN": "MINNESOTA TIMBERWOLVES", "NOP": "NEW ORLEANS PELICANS",
+    "OKC": "OKLAHOMA CITY THUNDER", "PHX": "PHOENIX SUNS", "POR": "PORTLAND TRAIL BLAZERS",
+    "SAC": "SACRAMENTO KINGS", "SAS": "SAN ANTONIO SPURS", "UTA": "UTAH JAZZ"
 }
 
 class GamePredictor:
